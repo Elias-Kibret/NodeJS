cf459affb9ddca73cad2cc8c0385f5af36fdb814
Elias <eliaskibret2013n2@gmail.com>
2022-04-12 12:14:07 +0300

    1.intofile

---
+++ app.js
@@ -0,0 +1,10 @@
+const amount=12;
+if(amount<10){
+    console.log("the amont is less than 12")
+
+
+}
+else{
+    console.log("the amount is greater than 12")
+}
+console.log(`hey the amount is ${amount}`)
