72432faa10df5cbb3ad0cb5e5c205dbca715c1a5
Elias <eliaskibret2013n2@gmail.com>
2022-04-12 12:24:30 +0300

    learn about how global varibles works

---
+++ 2-global.js
@@ -0,0 +1,17 @@
+// GLOBALS  - NO WINDOW !!!!
+
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function to use modules (CommonJS)
+// module     - info about current module (file)
+// process    - info about env where the program is being executed
+console.log(__dirname)
+//C:\Users\Elias Kibret\Desktop\React\nodecodeadic
+
+console.log(__filename)
+//C:\Users\Elias Kibret\Desktop\React\nodecodeadic\app.js
+
+setInterval(()=>{
+    console.log('Hello man')
+    //Print Hello man every second
+},1000)
